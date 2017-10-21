12e16aa36dbc27ddce4cae7fb17f4b6032ae9c2e
ryoyakawai <ryoya.kawai@gmail.com>
2017-10-21 00:36:53 -0700

    Add: index-jp.js, Remove: comented line

--- functions/index-jp.js
+++ functions/index-jp.js
@@ -5,6 +5,7 @@ const DialogflowApp = require('actions-on-google').DialogflowApp; // Google Assi
 
 const googleAssistantRequest = 'google'; // Constant to identify Google Assistant requests
 
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     console.log('Request headers: ' + JSON.stringify(request.headers));
     console.log('Request body: ' + JSON.stringify(request.body));
@@ -25,27 +26,24 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     let fact = 'Default Fact';
     let card = {src: null, alt: null};
     let factCategory = app.getArgument('fact-category');
-    
     const actionHandlers = {
         'tell_fact': () => {
             switch(factCategory) {
-            case 'history':
-            case 'past':
-                fact = 'Google Inc. is an American multinational technology company that specializes in Internet-related services and products. These include online advertising technologies, search, cloud computing, software, and hardware. Google was founded in 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University, in California.'; 
+            case '歴史':
+                fact = 'グーグルは、インターネット関連のサービスと製品に特化したアメリカの多国籍テクノロジー企業である。検索エンジン、オンライン広告、クラウドコンピューティング、ソフトウェア、ハードウェア関連の事業がある。スタンフォード大学の博士課程に在籍していたラリー・ペイジとセルゲイ・ブリンによって創業された。'; 
                 card ={src: 'http://162.243.3.155/wp-content/uploads/2016/03/wp-page_brin-COURTESY-GOOGLE.jpg',
                        alt: 'Larry Page & Sergey Brin'};
                 
                 break;
-            case 'headquarters':
-            case 'HQ':
-                fact = 'Google\'s headquarters in Mountain View, California, is referred to as the Googleplex, a play on words on the number googolplex and the headquarters itself being a complex of buildings. The lobby is decorated with a piano, lava lamps, old server clusters, and a projection of search queries on the wall.';
+            case '本社':
+                fact = 'グーグル本社はアメリカ合衆国カリフォルニア州マウンテンビューにあり、グーグルプレックスという愛称で親しまれている。愛称のグーグルプレックスは、10の10乗の更に100乗を意味するグーゴルプレックスに由来している。';
                 card ={src: 'https://www.solarpowerauthority.com/wp-content/uploads/solar-panels-on-the-googleplex.jpg',
                        alt: 'Googleplex'};
                 break;
             }
-            let factSpeech = 'This is ' + factCategory + ' about Google based on infomation on www.wikipedia.org on October twentieth 2017.' + fact + 'What would you like to hear about next? Google\'s history or headquarters? Or to quit, say bye.';
+            let factSpeech = 'グーグルの' + factCategory + 'は2017年10月21日のja.wikipedia.orgの情報です。' + fact + '次はグーグルの歴史とグーグルの本社のどちらを聞きたいですか？終了する場合は終了と指示してください。';
             if(app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)
-               && (card.src!=null && card.alt!=null) ) {
+               && (card.src!==null && card.alt!==null) ) {
                 app.ask(app.buildRichResponse()
                         .addSimpleResponse({speech: factSpeech, displayText: 'Google\'s ' + factCategory})
                         .addBasicCard(
